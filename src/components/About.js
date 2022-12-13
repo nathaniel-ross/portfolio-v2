3f097d54e64562a3f375ca18b0ff3aae7c2dd3f5
nathaniel-ross <nathaniel.rossloyo@gmail.com>
2022-12-13 14:34:41 -0800

    testing auto-deploy

--- src/components/About.js
+++ src/components/About.js
@@ -4,12 +4,13 @@ function About() {
 
     <div class="hero bg-base-300 mt-10 shadow-2xl">
     <div class="hero-content flex-col lg:flex-row">
-      <img src="https://nathaniel-ross.github.io/photos/me_photo.png" class="max-w-sm rounded-lg" alt="Nathaniel Ross-Loyo in a Chargers jersey"/>
+      <img src="https://nathaniel-ross.github.io/photos/me_photo.png" class="max-w-sm rounded-full shadow-2xl" alt="Nathaniel Ross-Loyo in a Chargers jersey"/>
       <div>
         <h1 class="text-5xl tracking-tight font-extrabold text-white">About Me</h1>
         <p class="py-7 text-2xl">Hi I'm Nathaniel, but I go by Nate! I've worked at a grocery store for the last 9 years, but in September 2022, I joined LEARN Academy which has given me the tools and knowledge to transition careers and become a Full-stack developer.</p>
         <p class="py-7 text-2xl">When I'm not coding I spend my free time building custom mechanical keyboards which is as time consuming as it is unnecesarily expensive. Other than that, you can catch me watching the Chargers play football every Sunday ⚡️</p>
         <p class="py-7 text-2xl">I plan to continue learning new languages to further expand my tech stack. So far I've been really enjoying the front-end side of styling and design, but the back-end has proved to be equally interesting</p>
+        <p>*--testing auto-deploy here--*</p>
       </div>
     </div>
   </div>
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,7 +4,7 @@ function Hero() {
   
     <div class="hero my-32 ">
     <div class="hero-content flex-col lg:flex-row-reverse mx-4">
-      <img src="https://nathaniel-ross.github.io/photos/me_drawing.png" class="max-w-sm mx-6 rounded-lg" alt="Drawing of Nathaniel"/>
+      <img src="https://nathaniel-ross.github.io/photos/me_drawing.png" class="max-w-sm mx-6 rounded-full shadow-2xl" alt="Drawing of Nathaniel"/>
       <div>
         <h1 class="text-5xl tracking-tight font-extrabold text-white">Hi, I'm Nathaniel!</h1>
         <p class="py-7 text-4xl">Full-stack developer with an emphasis on friendly design, creating responsive and dynamic web applications.</p>
