d89292c8b6fc8c0ee49643c64c43734495fa1283
nathaniel-ross <nathaniel.rossloyo@gmail.com>
2022-12-13 09:56:56 -0800

    fixed hero and title

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,12 +2,12 @@ function Hero() {
   return (
     <>
   
-    <div class="hero my-10 ">
+    <div class="hero my-32 ">
     <div class="hero-content flex-col lg:flex-row-reverse mx-4">
-      <img src="https://nathaniel-ross.github.io/photos/me_drawing.png" class="max-w-sm rounded-lg" />
+      <img src="https://nathaniel-ross.github.io/photos/me_drawing.png" class="max-w-sm mx-6 rounded-lg" alt="Drawing of Nathaniel"/>
       <div>
         <h1 class="text-5xl tracking-tight font-extrabold text-white">Hi, I'm Nathaniel!</h1>
-        <p class="py-7 text-3xl">Full-stack developer with an emphasis on friendly design. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam a placerat ex. Suspendisse tincidunt eleifend ante sed pretium. </p>
+        <p class="py-7 text-4xl">Full-stack developer with an emphasis on friendly design, creating responsive and dynamic web applications.</p>
         
         <button class="btn btn-secondary"><a href="#contact">Send me a message</a></button>
       </div>
