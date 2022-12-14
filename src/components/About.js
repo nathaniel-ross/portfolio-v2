98f9501ef704204f1edb2d53ad219a3d422c973e
nathaniel-ross <nathaniel.rossloyo@gmail.com>
2022-12-14 09:39:00 -0800

    got rid of placeholder

--- src/components/About.js
+++ src/components/About.js
@@ -10,7 +10,6 @@ function About() {
         <p class="py-7 text-2xl">Hi I'm Nathaniel, but I go by Nate! I've worked at a grocery store for the last 9 years, but in September 2022, I joined LEARN Academy which has given me the tools and knowledge to transition careers and become a Full-stack developer.</p>
         <p class="py-7 text-2xl">When I'm not coding I spend my free time building custom mechanical keyboards which is as time consuming as it is unnecesarily expensive. Other than that, you can catch me watching the Chargers play football every Sunday ⚡️</p>
         <p class="py-7 text-2xl">I plan to continue learning new languages to further expand my tech stack. So far I've been really enjoying the front-end side of styling and design, but the back-end has proved to be equally interesting</p>
-        <p>*--testing auto-deploy here--*</p>
       </div>
     </div>
   </div>
